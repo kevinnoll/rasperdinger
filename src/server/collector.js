f7bad17a913b9799196d797c884ba345a9e6439c
Kevin Noll <kevinnoll90@gmail.com>
2015-04-05 17:10:14 +0200

    [CHANGE] imported server application from https://github.com/kevinnoll/thresh

---
+++ src/server/collector.js
@@ -0,0 +1,115 @@
+module.exports = {
+	log : null,
+	pg : null,
+	client : null,
+	http : null,
+
+	init : function(http, log){
+		this.http = http;
+		this.log = log;
+		this.connectToDB();
+		this.collectSouls();
+	},
+
+	collectSouls : function(){
+		var that = this;
+		var now = new Date(); 
+		var yesterday = new Date(now.getTime() - 86400000); //subtract one day
+		var yesterdayMinusOneHour = new Date(yesterday.getTime() - 360000); //subtract one hour
+		var startTime = new Date(yesterdayMinusOneHour.getTime());
+		startTime.setHours(yesterdayMinusOneHour.getHours(), (5*Math.floor(yesterdayMinusOneHour.getMinutes()/5)), 0, 0);
+		this.collectKeys(startTime.getTime() / 1000);
+
+		setInterval(function(){
+			startTime.setTime(startTime.getTime() + 300000)
+			that.collectKeys(startTime.getTime() / 1000);
+		},300000); // 300000 for every 5 minutes
+		/*while(true){
+			//IDs first
+			if() // if now is 11:05:11 --> set time to 10:05:00 (minus 1 hour) and execute the request
+			this.collectKeys();
+			this.collectMatches();
+		}*/
+
+	},
+
+	collectKeys : function(timeToCollect){
+		this.log.info("collecting keys...");
+		var https = require('https');
+
+		var that = this;
+		var options = {
+			method: 'GET',
+			hostname: 'euw.api.pvp.net',
+			path: '/api/lol/euw/v4.1/game/ids?beginDate=' + timeToCollect + '&api_key=08d1d2cc-79c5-4dc2-9aa1-50b000cfcd20'
+		};
+		this.log.info(options.hostname +options.path);
+
+		var callback = function(response) {
+		  var str = '';
+		  
+		  //another chunk of data has been recieved, so append it to `str`
+		  response.on('data', function (chunk) {
+		    str += chunk;
+		  });
+
+		  //the whole response has been recieved, so we just print it out here
+		  response.on('end', function () {
+		    var aResponse = JSON.parse(str)
+		    that.persistIdsToDB(aResponse);
+		    that.log.info(str);
+		  });
+		}
+		var req = https.request(options, callback)
+		req.end();
+
+		req.on('error', function(e) {
+  			this.log.error(e);
+		});
+	},
+
+	persistIdsToDB : function(aIds){
+
+	},
+
+	collectMatches : function(){
+		var https = require('https');
+
+		var that = this;
+		var options = {
+			method: 'GET',
+			hostname: 'global.api.pvp.net',
+			path: '/api/lol/euw/v2.2/match/1778838158?api_key=08d1d2cc-79c5-4dc2-9aa1-50b000cfcd20'
+		};
+
+		var callback = function(response) {
+		  var str = '';
+		  
+		  //another chunk of data has been recieved, so append it to `str`
+		  response.on('data', function (chunk) {
+		    str += chunk;
+		  });
+
+		  //the whole response has been recieved, so we just print it out here
+		  response.on('end', function () {
+		    that.log.info(str);
+		    that.log.info("data completed");
+		  });
+		}
+		var req = https.request(options, callback)
+		req.end();
+
+		req.on('error', function(e) {
+  			this.log.error(e);
+		});
+		this.log.info("request sent");
+	},
+
+	connectToDB : function(){
+		var conString = "pg://thresh:thresh@localhost:5432/threshDB";
+		this.pg = require("pg")
+		this.client = new this.pg.Client(conString);
+		this.client.connect();
+		this.log.info("Connected to DB")
+	}
+}
---
+++ src/server/server.js
@@ -0,0 +1,22 @@
+var http = require("http")
+var Logger = require('basic-logger');
+var port = 5433;
+var host = 'localhost';
+
+var customConfig = {
+    showMillis: true,
+    showTimestamp: true
+};
+
+var log = new Logger(customConfig) // custom config parameters will be used, defaults will be used for the other parameters
+
+http.createServer(function(req, res) {
+	if(req.method == 'GET') {
+		log.info("GET request received");
+		//list_records(req,res);
+	}
+}).listen(port,host);
+log.info("##### Connected to " + port + "   " + host + " #####");
+
+var collector = require("./collector.js")
+collector.init(http, log);
