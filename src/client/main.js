29c6639df6e49af2bb2d45962a5517f363e591c4
Kevin Noll <kevinnoll90@gmail.com>
2015-04-11 10:01:16 +0200

    [FEATURE] added panning to graph, also some refactorings

--- src/client/main.js
+++ src/client/main.js
@@ -2,12 +2,12 @@
   "use strict";
 
     var rito = {
-        margin : null,
-      	width : null,
-      	height : null,
+        oMargin : null,
+      	iWidth : null,
+      	iHeight : null,
       	x : null,
       	y : null,
-        zoom : null,
+        oZoom : null,
       	xAxis : null,
       	yAxis : null,
       	svg : null,
@@ -25,7 +25,7 @@
 
         create : function(){
             this.createSVG();
-            this.createPlayers();
+            this.createWinrateBars();
         },
 
         loadData : function(){
@@ -44,8 +44,8 @@
               .append("svg:image")
               .attr('x',-10)
               .attr('y',-10)
-              .attr('width', 20)
-              .attr('height', 20)
+              .attr('iWidth', 20)
+              .attr('iHeight', 20)
               .attr("xlink:href",championImageUrl);
           });*/
         },
@@ -53,10 +53,10 @@
       	createScalesAndAxes : function(){
             var that = this;
             this.x = d3.scale.ordinal()
-                .rangeRoundBands([0, 4000], .1)
                 .domain(this.aChampionData.map(function(d) { return d.name; }))
+                .rangeRoundBands([0, 4000], .1);
             this.y = d3.scale.linear()
-                .range([this.height, 0])
+                .range([this.iHeight, 0])
                 .domain([0, d3.max(this.aChampionData, function(d) { return d.percentage; })]);
     	    this.xAxis = d3.svg.axis()
       			.scale(this.x)
@@ -65,32 +65,30 @@
       			.scale(this.y)
                 .orient("left")
                 .ticks(10, "%");
-            this.zoom = d3.behavior.zoom().scaleExtent([1, 1]);
-            this.zoom.x(this.x);
-            this.zoom.on('zoom', function() { 
-                var t = zoom.translate(),
-                tx = t[0],
-                ty = t[1];
+            this.oZoom = d3.behavior.zoom()
+                .scaleExtent([1, 1])
+                .on('zoom', $.proxy(this.draw,this));
 
-                tx = Math.min(tx, 0);
-                tx = Math.max(tx, width - max);
-                zoom.translate([tx, ty]);
-
-                svg.select('.data').attr('d', line);
-            });
+            //this.oZoom.x(this.x);
       	},
 
-      	createPlayers : function(){
+      	createWinrateBars : function(){
       		var that = this;
             var xPos = 0;
             $.get("http://localhost:5433", function( data ) {
                 //that.aChamionData = JSON.parse(data); 
                 that.aChampionData = _DATA_;
                 that.createScalesAndAxes();
+            
+                that.svg.call(that.oZoom)
+
+                that.field = that.svg.append("g")
+                    .classed(".field", true)
+                    .attr("transform", "translate(" + that.oMargin.left + "," + that.oMargin.top + ")")
 
                 that.field.append("g")
                     .attr("class", "x axis")
-                    .attr("transform", "translate(0," + that.height + ")")
+                    .attr("transform", "translate(0," + that.iHeight + ")")
                     .call(that.xAxis)
                         .selectAll("text")
                         .attr("y", -5)
@@ -116,21 +114,32 @@
                     .attr("x", function(d) { return that.x(d.name); })
                     .attr("width", that.x.rangeBand())
                     .attr("y", function(d) { return that.y(d.percentage); })
-                    .attr("height", function(d) { return that.height - that.y(d.percentage); });
+                    .attr("height", function(d) { return that.iHeight - that.y(d.percentage); });
             });  		
       	},
 
       	createSVG : function(){
-            this.margin = {top: 20, right: 20, bottom: 30, left: 40};
-            this.width = 1200 - this.margin.left - this.margin.right;
-            this.height = 500 - this.margin.top - this.margin.bottom;
+            this.oMargin = {top: 20, right: 20, bottom: 30, left: 40};
+            this.iWidth = 1200 - this.oMargin.left - this.oMargin.right;
+            this.iHeight = 500 - this.oMargin.top - this.oMargin.bottom;
           	this.svg = d3.select("body").append("svg")
-        		.attr("width", this.width + this.margin.left + this.margin.right)
-        		.attr("height", this.height + this.margin.top + this.margin.bottom);
-            this.field = this.svg.append("g")
-                .classed(".field", true)
-                .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-       	}
+        		.attr("width", this.iWidth + this.oMargin.left + this.oMargin.right)
+        		.attr("height", this.iHeight + this.oMargin.top + this.oMargin.bottom);
+       	},
+
+        draw : function(){
+            console.log("zooming");
+            /*t = this.oZoom.translate(),
+            tx = t[0],
+            ty = t[1];
+
+            tx = Math.min(tx, 0);
+            tx = Math.max(tx, that.iWidth - 4000);
+            this.oZoom.translate([tx, ty]);*/
+            d3.select(".x.axis").attr("transform", "translate(" + d3.event.translate[0]+","+(this.iHeight)+")")
+            d3.selectAll(".bar").attr("transform", "translate(" + d3.event.translate[0]+",0)scale(" + d3.event.scale + ",1)");
+
+        }
 
     }
 
