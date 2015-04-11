a1000a07ade10f1a93b2e385476d08c47dca0152
Kevin Noll <kevinnoll90@gmail.com>
2015-04-11 08:31:30 +0200

    [FEATURE] implemented get request

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,4 @@
-var http = require("http")
+var http = require("http");
 var Logger = require('basic-logger');
 var port = 5433;
 var host = 'localhost';
@@ -9,11 +9,13 @@ var customConfig = {
 };
 
 var log = new Logger(customConfig) // custom config parameters will be used, defaults will be used for the other parameters
+var rest = require("./rest.js"); 
 
 http.createServer(function(req, res) {
 	if(req.method == 'GET') {
 		log.info("GET request received");
 		//list_records(req,res);
+		rest.getWinRates(res, req, http, log);
 	}
 }).listen(port,host);
 log.info("##### Connected to " + port + "   " + host + " #####");
