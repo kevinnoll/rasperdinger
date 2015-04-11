e27408e200a276f70a922a7b66a5bec650faa4b8
Kevin Noll <kevinnoll90@gmail.com>
2015-04-11 08:31:47 +0200

    [FEATURE] changed client to display winrates

--- src/client/main.js
+++ src/client/main.js
@@ -2,6 +2,7 @@
   "use strict";
 
   var rito = {
+    margin : null,
   	width : null,
   	height : null,
   	x : null,
@@ -13,115 +14,109 @@
   	data : null,
   	mChampionNames : null,
   	mChampionImageUrls : null,
+    aWinRates : null,
+    aChampionData : null,
 
   	init : function(){
-  		this.loadImages();
-  		this.setup();
+  		this.loadData();
   		this.create();
   	},
 
-  	loadImages : function(){
-  		var that = this;
-  		this.mChampionNames = {};
-  		this.mChampionImageUrls = {};
-  		$.each(_DATA_.participants,function(index,value){
-			$.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/" + value.championId + "?champData=info&api_key=08d1d2cc-79c5-4dc2-9aa1-50b000cfcd20", function( data ) {
-			  that.mChampionNames[value.participantId] = data.key;
-			  var championImageUrl = "http://ddragon.leagueoflegends.com/cdn/5.6.1/img/champion/" + data.key + ".png";
-			  that.mChampionImageUrls[value.participantId] = championImageUrl;
-			  d3.select(".player_" + value.participantId)
-			  	.append("svg:image")
-	   			.attr('x',-10)
-	   			.attr('y',-10)
-	   			.attr('width', 20)
-	   			.attr('height', 20)
-	   			.attr("xlink:href",championImageUrl);
-			});
-  		})
- 
-  	},
+    create : function(){
+      this.createSVG();
+      this.createPlayers();
+    },
 
-  	setup : function(){
-  		this.width = 800;
-  		this.height = 800;
-  		this.x = d3.scale.linear()
-  			.domain([0,15000])
-  			.range([0, this.width]);
-  		this.y = d3.scale.linear()
-			.domain([0,15000])
-  			.range([this.height, 0]);
+    loadData : function(){
+      this.loadWinRates();
+    },
+
+    loadWinRates : function(){
+      var that = this;
+      this.aWinRates = [];
+      /*$.get("http://localhost:5433", function( data ) {
+      //$.get("http://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/" + value.championId + "?champData=info&api_key=08d1d2cc-79c5-4dc2-9aa1-50b000cfcd20", function( data ) {
+        that.mChampionNames[value.participantId] = data.key;
+        var championImageUrl = "http://ddragon.leagueoflegends.com/cdn/5.6.1/img/champion/" + data.key + ".png";
+        that.mChampionImageUrls[value.participantId] = championImageUrl;
+        d3.select(".player_" + value.participantId)
+          .append("svg:image")
+          .attr('x',-10)
+          .attr('y',-10)
+          .attr('width', 20)
+          .attr('height', 20)
+          .attr("xlink:href",championImageUrl);
+      });*/
+    },
+
+  	createScalesAndAxes : function(){
+      this.x = d3.scale.ordinal()
+        .rangeRoundBands([0, this.width], .1)
+        .domain(this.aChampionData.map(function(d) { return d.name; }))
+      this.y = d3.scale.linear()
+        .range([this.height, 0])
+        .domain([0, d3.max(this.aChampionData, function(d) { return d.percentage; })]);
   		this.xAxis = d3.svg.axis()
-  			.scale(this.x);
+  			.scale(this.x)
+        .orient("bottom");
   		this.yAxis = d3.svg.axis()
-  			.scale(this.y);
-  	},
-
-  	create : function(){
-  		this.createSVG();
-  		this.createBackgroundImage();
-  		this.createField();
-  		this.createPlayers();
+  			.scale(this.y)
+        .orient("left")
+        .ticks(10, "%");
   	},
 
   	createPlayers : function(){
   		var that = this;
-  		var aFrames = _DATA_.timeline.frames;
-  		var aPlayers = _DATA_.participants;
-  		var oPlayersInitial = aFrames[0].participantFrames;
-  		var aPlayersInitial = $.map(oPlayersInitial, function(value, index) {
-    		return [value];
-		});
+      var xPos = 0;
+      $.get("http://localhost:5433", function( data ) {
+        //that.aChamionData = JSON.parse(data); 
+        that.aChampionData = _DATA_;
+        that.createScalesAndAxes();
 
-  		var oPlayers = this.field.selectAll(".player")
-  			.data(aPlayersInitial)
-  			.enter()
-  			.append("g")
-  			.attr("class", function(d){
-  				return "player_"+d.participantId;
-  			})
-  			.attr("transform", function(d){
-  				return "translate(" + that.x(d.position.x) + "," + that.y(d.position.y) + ")";
-  			});
+        that.field.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + that.height + ")")
+          .call(that.xAxis)
+            .selectAll("text")
+            .attr("y", -5)
+            .attr("transform", "rotate(-90)");
 
-  		oPlayers.append("circle")
-            .attr("x", 7)
-            .attr("y", 7)
-            .attr("r", 5)
-            .style("fill", function(d){
-            	for(var i = 0; i < aPlayers.length; i++){
-            		if(aPlayers[i].participantId === d.participantId){
-            			if(aPlayers[i].teamId === 100){
-            				return "red";
-            			} else {
-            				return "blue";
-            			}
-            		}
-            	}
-            })
-            .style("stroke-width", 1)
-            .style("stroke","black");
-  	},
+        that.field.append("g")
+          .attr("class", "y axis")
+          .call(that.yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Frequency");
 
-  	createBackgroundImage : function(){
-  		this.svg.append("svg:image")
-   			.attr('x',-15)
-   			.attr('y',5)
-   			.attr('width', 800)
-   			.attr('height', 800)
-   			.attr("xlink:href","img/map.jpg")
+        var oChampionWinrates = that.field.selectAll(".bar")
+          .data(that.aChampionData)
+          .enter()
+          .append("rect")
+          .attr("class", function(d){
+            return "bar winrate_"+d.name;
+           })
+          .attr("x", function(d) { return that.x(d.name); })
+          .attr("width", that.x.rangeBand())
+          .attr("y", function(d) { return that.y(d.percentage); })
+          .attr("height", function(d) { return that.height - that.y(d.percentage); });
+      });  		
   	},
 
   	createSVG : function(){
+      this.margin = {top: 20, right: 20, bottom: 30, left: 40};
+      this.width = 1200 - this.margin.left - this.margin.right;
+      this.height = 500 - this.margin.top - this.margin.bottom;
   		this.svg = d3.select("body").append("svg")
-    		.attr("width", this.width)
-    		.attr("height", this.height);
-  	},
-
-  	createField : function(){
-		this.field = this.svg.append("g")
-  			.classed(".field", true);
-
+    		.attr("width", this.width + this.margin.left + this.margin.right)
+    		.attr("height", this.height + this.margin.top + this.margin.bottom);
+      this.field = this.svg.append("g")
+        .classed(".field", true)
+        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
   	}
+
   }
 
   rito.init()
