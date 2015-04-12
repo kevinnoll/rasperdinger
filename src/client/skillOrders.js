1c12cbbf69be5922d80e29bfd02bfac46f1666f2
Kevin Noll <kevinnoll90@gmail.com>
2015-04-12 13:21:16 +0200

    [FEATURE] added a grid of champ images

--- src/client/skillOrders.js
+++ src/client/skillOrders.js
@@ -1,10 +1,57 @@
 (function(){
   "use strict";
 
-    var rito = {
+    var skillOrder = {
+    	aChamps : null,
+
     	init : function(){
+    		var that = this;
+    		$.get("http://localhost:5433/winrates", function(winrates) {
+                that.aChamps = JSON.parse(winrates);  
+                that.createGrid();
+            });
+    	},
+
+    	createGrid : function(){
+    		var flip = d3.select(".svg_anchor").selectAll(".not_existent")
+    			.data(this.aChamps)
+    			.enter()
+				.append("div")
+    			.attr("class",function(d){
+    				return "flip floated_img img "+d.name;
+    			})
+    			.style("height", "48px")
+    			.style("width", "48px")
+    		
+    		var card = flip.append("div")
+    				.classed("card", true);
+
+    		card.append("div")
+    					.classed("face front", true)
+		    			.append("img")
+		    			.attr("src", function(d){
+		    				return "http://ddragon.leagueoflegends.com/cdn/5.7.2/img/champion/"+d.name+".png";
+		    			})
+		    			.attr("alt", function(d){
+		    				return d.name;
+		    			})
+		    			.style("max-width", "100%")
+		    			.style("max-height", "100%")
+		    			.on("click", function(d){
+		    				debugger;
+		    				$(this).find('.card').addClass('flipped').mouseleave(function(){
+					            $(this).removeClass('flipped');
+					        });
+					        return false;
+		    			});
+
+		    card.append("div")
+		    	.classed("face back", true);
+
+    		this.aChamps = null;
+    		SequenceDiagram.create();
     	}
     }
 
-    rito.init()
+    skillOrder.init()
 })();
