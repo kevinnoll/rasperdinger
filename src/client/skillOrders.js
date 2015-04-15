49a6493e6655005fe7a327caa491446d6c75b0e2
Kevin Noll <kevinnoll90@gmail.com>
2015-04-15 19:29:56 +0200

    [REFACTORING] removed unused stuff

--- src/client/skillOrders.js
+++ src/client/skillOrders.js
@@ -13,7 +13,7 @@
     	},
 
     	createGrid : function(){
-    		var flip = d3.select(".svg_anchor").selectAll(".not_existent")
+    		var divSel = d3.select(".svg_anchor").selectAll(".not_existent")
     			.data(this.aChamps)
     			.enter()
 				.append("div")
@@ -22,37 +22,26 @@
     			})
     			.style("height", "48px")
     			.style("width", "48px")
-    		
-    		var card = flip.append("div")
-    				.classed("card", true);
 
-    		card.append("div")
-    					.classed("face front", true)
-		    			.append("img")
-		    			.attr("src", function(d){
-		    				return "http://ddragon.leagueoflegends.com/cdn/5.7.2/img/champion/"+d.name+".png";
-		    			})
-		    			.attr("alt", function(d){
-		    				return d.name;
-		    			})
-		    			.style("max-width", "100%")
-		    			.style("max-height", "100%")
-		    			.on("click", function(d){
-		    				$(this).find('.card').addClass('flipped').mouseleave(function(){
-					            $(this).removeClass('flipped');
-					        });
+    		divSel.append("img")
+    			.attr("src", function(d){
+    				return "http://ddragon.leagueoflegends.com/cdn/5.7.2/img/champion/"+d.name+".png";
+    			})
+    			.attr("alt", function(d){
+    				return d.name;
+    			})
+    			.style("max-width", "100%")
+    			.style("max-height", "100%")
+    			.on("click", function(d){
 
-					        d3.selectAll(".floated_img").classed("css3grayscale1",true);
-					        d3.select(".floated_img."+d.name).classed("css3grayscale1", false);
-					        d3.select("#container").selectAll("*").remove();
-					        d3.select("#sequence").selectAll("*").remove();
-					        d3.select("#legend").selectAll("*").remove();
-					        SequenceDiagram.create(d);
-					        return false;
-		    			});
-
-		    card.append("div")
-		    	.classed("face back", true);
+			        d3.selectAll(".floated_img").classed("css3grayscale1",true);
+			        d3.select(".floated_img."+d.name).classed("css3grayscale1", false);
+			        d3.select("#container").selectAll("*").remove();
+			        d3.select("#sequence").selectAll("*").remove();
+			        d3.select("#legend").selectAll("*").remove();
+			        SequenceDiagram.create(d);
+			        return false;
+    			});
 
     		this.aChamps = null;
     	}
